54d4409f8b22eee659be6d6d28a2a6db792f60b6
Mike Meredith <mmeredith@wcs.org>
2013-08-22 00:00:00 +0000

    version 0.2.0

--- R/BESTpower.R
+++ R/BESTpower.R
@@ -2,7 +2,7 @@ BESTpower <-
 function( BESTobj, N1, N2, credMass=0.95, ROPEm, ROPEsd, ROPEeff,
                      maxHDIWm, maxHDIWsd, maxHDIWeff, compValm=0, nRep=200,
                      mcmcLength=10000, saveName=NULL,
-                     showFirstNrep=0 ) {
+                     showFirstNrep=0, verbose=2, rnd.seed=NULL) {
   # This function estimates power.
  
   # Sanity checks:
@@ -29,7 +29,6 @@ function( BESTobj, N1, N2, credMass=0.95, ROPEm, ROPEsd, ROPEeff,
       N2 <- length(attr(BESTobj, "data")$y2)
     N2 <- rep(N2, length.out=nRep)
   }
-
 
   # Deal with missing or invalid arguments for criteria:
   wanted <- rep(TRUE, 12)
@@ -60,6 +59,14 @@ function( BESTobj, N1, N2, credMass=0.95, ROPEm, ROPEsd, ROPEeff,
   if(!any(wanted))
     stop("No valid criteria set.")
 
+  # Deal with random number seeds
+  if(!is.null(rnd.seed)) {
+    set.seed(rnd.seed[1], "Mersenne-Twister")
+    if(length(rnd.seed) != nRep)
+      rnd.seed <- sample.int(1e6, nRep)
+    on.exit(set.seed(NULL, "default"))
+  }
+
   # Select thinned steps in chain for posterior predictions:
   stepIdxVec = seq( 1 , chainLength , floor(chainLength/nRep) )[1:nRep]
   paramDF <- BESTobj[stepIdxVec, ]
@@ -80,12 +87,18 @@ function( BESTobj, N1, N2, credMass=0.95, ROPEm, ROPEsd, ROPEeff,
     "effect:   HDI < ROPE",
     "effect:  HDI in ROPE",
     "effect: HDI width ok")
-
+    
+  if(verbose == 1)  {
+    pb <- txtProgressBar(style = 3)
+    on.exit(close(pb))
+  }
   for (i in 1:nRep) {
-    cat( "\n:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" )
-    cat( paste( "Power computation: Simulated Experiment" , i , "of" , 
-                nRep , ":\n\n" ) )
-    flush.console()
+    if(verbose > 1) {
+      cat( "\n:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" )
+      cat( paste( "Power computation: Simulated Experiment" , i , "of" , 
+                  nRep , ":\n\n" ) )
+      flush.console()
+    }
     # Get parameter values for this simulation:
     if(oneGrp) {
       mu1Val = paramDF[i,"mu"]
@@ -101,12 +114,14 @@ function( BESTobj, N1, N2, credMass=0.95, ROPEm, ROPEsd, ROPEeff,
     y1 <- rt(N1[i], df=nuVal) * sigma1Val + mu1Val    
     y2 <- if(oneGrp) NULL else rt(N2[i], df=nuVal) * sigma2Val + mu2Val    
     # Get posterior for simulated data:
-    simChain <- BESTmcmc( y1, y2, numSavedSteps=mcmcLength, thinSteps=1)
+    simChain <- BESTmcmc( y1, y2, numSavedSteps=mcmcLength, thinSteps=1,
+                          verbose=verbose > 1, rnd.seed=rnd.seed[i])
     if (i <= showFirstNrep ) { 
-      x11()  # Changed 09-03-2013
-      # dev.new() # Doesn't work in Rstudio ??
+      # x11()   # Deprecated as "platform specific" in R 3.1
+      dev.new() # Doesn't work properly in Rstudio: gives warning and plots to old device.
       plotAll(simChain, ROPEm=ROPEm, ROPEsd=ROPEsd, ROPEeff=ROPEeff,
               compValm=compValm) 
+      mtext(paste("Simulation", i), outer=TRUE, line=-1, font=4)
     }
     #simChain <- as.matrix(Bout)
     # Get the HDIs for each parameter:
@@ -145,10 +160,15 @@ function( BESTobj, N1, N2, credMass=0.95, ROPEm, ROPEsd, ROPEeff,
     for ( j in which(wanted)) {
       power[j, 2:3] = hdi( qbeta , shape1=s1[j] , shape2=s2[j] )
     }
-    cat( "\nAfter", i, "Simulated Experiments, Posterior Probability
-       of meeting each criterion is (mean and 95% CrI):\n" )
-    print(round(power[wanted, ], 3))
-    flush.console()
+    if(verbose > 1) {
+      cat( "\nAfter", i, "Simulated Experiments, Posterior Probability
+         of meeting each criterion is (mean and 95% CrI):\n" )
+      print(round(power[wanted, ], 3))
+      flush.console()
+    }
+    if(verbose == 1)
+      setTxtProgressBar(pb, i/nRep)
+      
     if(!is.null(saveName))
       save( i , power , file=saveName )
   }
